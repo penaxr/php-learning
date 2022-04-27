6b76d5e9a6a478859f65fc7a816285ba1ba6c69d
penaxr <yannsd0728@gmail.com>
2022-04-26 22:11:28 -0400

    learnphp.sql

--- ytdir/includes/learnphp.sql
+++ ytdir/includes/learnphp.sql
@@ -1,4 +1,5 @@
 CREATE TABLE `ytdir` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `url` varchar(30) NOT NULL,
+  `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
