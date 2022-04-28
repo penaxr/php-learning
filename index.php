4c1f64ac9cdc9ad056e78557edcebc21e83003c6
penaxr <yannsd0728@gmail.com>
2022-04-28 12:55:58 -0400

    Added git/dev urls

--- index.php
+++ index.php
@@ -30,75 +30,87 @@
 <a href=""></a>
 
 <body class="bg-dark">
-<br>
-<br>
-    <div class="container col-md-5 bg-light bg-gradient mt-3 shadow-lg p-3  bg-light rounded">
+    <br>
+    <br>
+
+    <div class="container col bg-light bg-gradient mt-3 shadow-lg p-3  bg-light rounded">
         <div class="card">
             <div class="card-header">
-                <h3 class="text-center font-monospace">About the project</h3>
+                <h3 class="text-center font-monospace"><i class="fa-solid fa-file-code"> About the project</i></h3>
             </div>
             <div class="card-body">
                 <h5 class="card-title">A journey into PHP</h5>
-                <p class="card-text">This is my first project made with php. A simple youtube video url database, But I plan to add more features as I learn PHP</p>
-                <a href="https://github.com/penaxr/php-learning" class="btn btn-primary"><i class="fa-brands fa-github"></i> My Github</a>
+                <!--<p class="card-text">This is my first project made with php. A simple youtube video url database, But I plan to add more features as I learn PHP</p>-->
+                <p class="card-text">I decided it was time to learn PHP after doing only frontend for a while. My goal is to be able to rely on myself for my own projects.</p>
+                <p class="card-text">My goal is to be able to rely on myself for my own projects.</p>
+                <ul>
+                    <li>learn via w3school and php.net</li>
+                    <li>Create simple functions for myself</li>
+                    <li>Have fun, Learn and learn even more</li>
+                </ul>
+
+                <a href="https://github.com/penaxr/php-learning" class="btn btn-dark"><i class="fa-brands fa-github"></i> My Github</a>
+                <a href="https://dev.to/penaxr" class="btn btn-dark"><i class="fa-brands fa-dev"></i> DEV.TO</a>
             </div>
         </div>
-    </div>
 
-    <div class="container col-md-5 bg-light bg-gradient mt-3 shadow-lg p-3  bg-light rounded">
-        <div class="col-auto col-md-offset-1 ">
-            <div class="alert alert-info" role="alert">
-                <strong> <i class="fa-solid fa-dragon"></i> simple youtube url manager </strong>
-            </div>
-            <div class="panel panel-default panel-table">
-                <div class="panel-heading ">
-                    <div class="row">
-
-                        <script>
-                            var alertList = document.querySelectorAll('.alert');
-                            alertList.forEach(function(alert) {
-                                new bootstrap.Alert(alert)
-                            })
-                        </script>
-                    </div>
-                    <form class="form" action="addurl.php" method="post">
-                        <div class="row g-3 align-items-center">
-                            <div class="col-auto">
-                                <label for="title" class="col-form-label">Title: </label>
-                            </div>
-                            <div class="col-auto">
-                                <div class="form-group">
-                                    <input type="text" class="form-control" name="title" id="title" placeholder="" style="width: 173px;" required>
-
-                                </div>
-                            </div>
-                            <div class="col-auto">
-                                <div class="input-group">
-                                    <span class="input-group-text" id="youtubeurl">https://www.youtube.com/watch?v=</span>
-                                    <input type="text" class="form-control" placeholder="" name="url" aria-describedby="youtubeurl" required>
-                                    <button class="btn btn-success" name="AddUrl" type="submit"> <i class="fa-solid fa-circle-plus"></i></button>
-                                </div>
-                            </div>
-                        </div>
-
-                    </form>
+        <div class="container col bg-light bg-gradient mt-3 shadow-lg p-3  bg-light rounded">
+            <div class="col-auto col-md-offset-1 ">
+                <div class="alert alert-dark" role="alert">
+                    <strong> <i class="fa-solid fa-dragon"></i> simple youtube url manager </strong>
                 </div>
-                <div class="col-auto py-1">
-                    <div class="panel-body">
-                        <table class="table table-striped table-bordered table-list">
-                            <?php
+                <div class="panel panel-default panel-table">
+                    <div class="panel-heading ">
+                        <div class="row">
 
-                            // setup la db
-                            require_once('includes/dbconf.php');
+                            <script>
+                                var alertList = document.querySelectorAll('.alert');
+                                alertList.forEach(function(alert) {
+                                    new bootstrap.Alert(alert)
+                                })
+                            </script>
+                        </div>
+                        <form class="form" action="addurl.php" method="post">
+                            <div class="row g-3 align-items-center">
+                                <div class="col-auto">
+                                    <label for="title" class="col-form-label"><i class="fa-solid fa-file-signature"> Title</i></label>
+                                </div>
+                                <div class="col-auto">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="title" id="title" placeholder="" style="width: 173px;" required>
 
-                            // variables de la db
-                            $ytdb = "SELECT id, url, name, fav FROM ytdir";
-                            $urldata = mysqli_query($conn, $ytdb);
+                                    </div>
+                                </div>
+                                <div class="col-auto">
+                                    <label for="title" class="col-form-label"><i class="fa-solid fa-hashtag"> Video ID</i></label>
+                                </div>
+                                <div class="col-auto">
+                                    <div class="input-group">
+                                        <span class="input-group-text" id="youtubeurl">https://www.youtube.com/watch?v=</span>
+                                        <input type="text" class="form-control" placeholder="" name="url" aria-describedby="youtubeurl" required>
+                                        <button class="btn btn-success" name="AddUrl" type="submit"> <i class="fa-solid fa-circle-plus"></i></button>
+                                    </div>
+                                </div>
+                            </div>
 
-                            // verifications et requetes
-                            if (mysqli_num_rows($urldata) > 0) {
-                                echo
-                                "
+                        </form>
+                    </div>
+                    <div class="col-auto py-1">
+                        <div class="panel-body">
+                            <table class="table table-striped table-bordered table-list">
+                                <?php
+
+                                // setup la db
+                                require_once('includes/dbconf.php');
+
+                                // variables de la db
+                                $ytdb = "SELECT id, url, name, fav FROM ytdir";
+                                $urldata = mysqli_query($conn, $ytdb);
+
+                                // verifications et requetes
+                                if (mysqli_num_rows($urldata) > 0) {
+                                    echo
+                                    "
                             <thead>
                             <tr>
                             <th>VIDEO TITLE</th>
@@ -108,30 +120,30 @@
                             </thead>";
 
 
-                                while ($row = mysqli_fetch_array($urldata)) {
-                                    echo '<form class="form" method="post">';
-                                    echo "<tr>";
-                                    echo "<td style='width: 200px'>" . $row['name'] . "</td>";
-                                    echo "<td style='width: 135px'>" . $row['url'] . "</td>";
-                                    echo '<td style="width: 165px;"><button class="btn btn-info" formaction="https://www.youtube.com/watch?v=' . $row['url'] . '" name="favbtn"><i class="fa-solid fa-eye"> Watch</i></button><button class="btn btn-danger ms-2" type="submit" name="delbtn" value="' . $row['id'] . '" /><i class="fa-solid fa-ban"> Delete</i></button></td>';
-                                    echo "</tr>";
-                                    echo '</form>';
+                                    while ($row = mysqli_fetch_array($urldata)) {
+                                        echo '<form class="form" method="post">';
+                                        echo "<tr>";
+                                        echo "<td style='width: 200px'>" . $row['name'] . "</td>";
+                                        echo "<td style='width: 135px'>" . $row['url'] . "</td>";
+                                        echo '<td style="width: 165px;"><button class="btn btn-info" formaction="https://www.youtube.com/watch?v=' . $row['url'] . '" name="favbtn"><i class="fa-solid fa-eye"> Watch</i></button><button class="btn btn-danger ms-2" type="submit" name="delbtn" value="' . $row['id'] . '" /><i class="fa-solid fa-ban"> Delete</i></button></td>';
+                                        echo "</tr>";
+                                        echo '</form>';
+                                    }
+
+                                    echo "</table>";
+
+                                    if (isset($_POST['delbtn']) and is_numeric($_POST['delbtn'])) {
+                                        $delete = $_POST['delbtn'];
+                                        $ytdel =  "DELETE FROM `ytdir` where `id` = '$delete'";
+                                        mysqli_query($conn, $ytdel);
+                                    }
+
+                                    mysqli_close($conn);
                                 }
 
-                                echo "</table>";
+                                ?>
 
-                                if (isset($_POST['delbtn']) and is_numeric($_POST['delbtn'])) {
-                                    $delete = $_POST['delbtn'];
-                                    $ytdel =  "DELETE FROM `ytdir` where `id` = '$delete'";
-                                    mysqli_query($conn, $ytdel);
-                                }
-
-                                mysqli_close($conn);
-                            }
-
-                            ?>
-
-                            <!--        <nav aria-label="Page navigation pt-4">
+                                <!--        <nav aria-label="Page navigation pt-4">
                                 <ul class="pagination justify-content-center|end">
                                     <li class="page-item disabled">
                                         <a class="page-link" href="#" aria-label="Previous">
@@ -150,13 +162,19 @@
                                 </ul>
                             </nav> -->
 
+                        </div>
+
                     </div>
-
                 </div>
-            </div>
 
+            </div>
         </div>
+
+
+
     </div>
+
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
